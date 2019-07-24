13a3d0a0b39b032c84c4ba69b24183dd2d3612ca
Taifendesune <1970024754@qq.com>
2019-07-24 15:52:56 +0800

    修改了输入框的显示问题

--- src/main/java/com/javaee/hotel/controller/IndexController.java
+++ src/main/java/com/javaee/hotel/controller/IndexController.java
@@ -1,12 +1,26 @@
 package com.javaee.hotel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("welcome")
 public class IndexController {
-  @GetMapping(value="")
+    @GetMapping(value = "")
     public String showIndex () {return "index";}
+    @PostMapping(value = "")
+    public String searchHotel(HttpServletRequest request, Model model){
+      String hotelname = request.getParameter("hotelname");
+      String indate = request.getParameter("checkindate");
+      String outdate = request.getParameter("checkoutdate");
+      String starlevel = request.getParameter("starlevel");
+      System.out.println(hotelname+"-"+indate+"-"+outdate+"-"+starlevel);
+
+      return "index";
+    }
 }
