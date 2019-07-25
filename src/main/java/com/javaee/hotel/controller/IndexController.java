f5719c986950a655a87ca7d6732649df7cea0c2d
Taifendesune <1970024754@qq.com>
2019-07-25 17:05:53 +0800

    酒店名称查找初步实现

--- src/main/java/com/javaee/hotel/controller/IndexController.java
+++ src/main/java/com/javaee/hotel/controller/IndexController.java
@@ -1,26 +1,36 @@
 package com.javaee.hotel.controller;
 
+import com.javaee.hotel.service.RoomService;
+import com.javaee.hotel.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("welcome")
 public class IndexController {
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private SearchService searchService;
+
     @GetMapping(value = "")
-    public String showIndex () {return "index";}
+    public String showIndex (Model model) {
+        model.addAttribute("hotels", roomService.getHotelList());
+        return "index";
+    }
     @PostMapping(value = "")
     public String searchHotel(HttpServletRequest request, Model model){
-      String hotelname = request.getParameter("hotelname");
-      String indate = request.getParameter("checkindate");
-      String outdate = request.getParameter("checkoutdate");
-      String starlevel = request.getParameter("starlevel");
-      System.out.println(hotelname+"-"+indate+"-"+outdate+"-"+starlevel);
-
-      return "index";
+        String hotelname = request.getParameter("hotelname");
+        String indate = request.getParameter("checkindate");
+        String outdate = request.getParameter("checkoutdate");
+        String starlevel = request.getParameter("starlevel");
+        System.out.println(hotelname+"-"+indate+"-"+outdate+"-"+starlevel);
+        model.addAttribute("hotels",searchService.searchh(hotelname));
+        return "index";
     }
 }
---
+++ src/main/java/com/javaee/hotel/service/SearchService.java
@@ -0,0 +1,25 @@
+package com.javaee.hotel.service;
+
+import com.javaee.hotel.domain.Hotel;
+import com.javaee.hotel.domain.HotelExample;
+import com.javaee.hotel.mapper.HotelMapper;
+import com.javaee.hotel.mapper.HotelMapperExt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class SearchService {
+    @Autowired
+    private HotelMapper hotelMapper;
+
+    public List<Hotel> searchh(String keyword){
+        HotelExample example=new HotelExample();
+        example.createCriteria().andHotelChineseNameLike(keyword);
+        return hotelMapper.selectByExample(example);
+    }
+
+}
