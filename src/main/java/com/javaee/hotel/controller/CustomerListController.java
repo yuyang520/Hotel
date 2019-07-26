94f48b0b19e0e388fb4d24c803a708ea5af9ae84
525438796 <525438796@qq.com>
2019-07-26 16:57:59 +0800

    添加管理员对用户信息进行修改的页面

--- src/main/java/com/javaee/hotel/controller/CustomerListController.java
+++ src/main/java/com/javaee/hotel/controller/CustomerListController.java
@@ -1,14 +1,14 @@
 package com.javaee.hotel.controller;
 
 
+import com.javaee.hotel.domain.Customer;
 import com.javaee.hotel.domain.CustomerInfo;
 import com.javaee.hotel.service.CustomerListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,8 +26,40 @@ public class CustomerListController {
     @GetMapping(value = "/customerList.json", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<CustomerInfo> getCustomerInfoJson(){
+
         return customerListService.customerInfoRespond();
     }
 
+    @GetMapping("/add")
+    public String goCustomerAddPage(){
+        return "customer-add";
+    }
 
+    @PostMapping("/add")
+    public String saveCustomerList(CustomerInfo customerInfo){
+        customerListService.saveCustomerList(customerInfo);
+
+        return "redirect:/customerList";
+    }
+
+//    @GetMapping("/edit")
+//    public String goCustomerListEdit(@RequestParam("id") int id , Model model){
+//        Customer cus = customerListService.findCustomerInfoByPrimaryKey(id);
+//        model.addAttribute("customer",cus);
+//
+//        return "customer-add";
+//    }
+
+    @GetMapping("/edit")
+    public String test(@RequestParam("id") int id , Model model) {
+        CustomerInfo customerInfo = customerListService.getCustomerInfo(id);
+        if(customerInfo != null){
+            model.addAttribute("customerInfo",customerInfo);
+            return "customer-add";
+        }
+        else{
+            return null;
+        }
+
+    }
 }
--- src/main/java/com/javaee/hotel/service/CustomerListService.java
+++ src/main/java/com/javaee/hotel/service/CustomerListService.java
@@ -1,11 +1,15 @@
 package com.javaee.hotel.service;
 
 
+import com.javaee.hotel.domain.Customer;
+import com.javaee.hotel.domain.CustomerExample;
 import com.javaee.hotel.domain.CustomerInfo;
 import com.javaee.hotel.domain.CustomerInfoExample;
 import com.javaee.hotel.mapper.CustomerInfoMapper;
+import com.javaee.hotel.mapper.CustomerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,6 +17,8 @@ import java.util.List;
 public class CustomerListService {
     @Autowired
     private CustomerInfoMapper customerInfoMapper;
+    @Autowired
+    private CustomerMapper customerMapper;
 
     public List<CustomerInfo> customerInfoRespond(){
         CustomerInfoExample example = new CustomerInfoExample();
@@ -20,4 +26,25 @@ public class CustomerListService {
     }
 
 
+    public Integer saveCustomerList(CustomerInfo customerInfo){
+        return customerInfoMapper.insert(customerInfo);
+    }
+
+    public Customer findCustomerInfoByPrimaryKey(int id){
+
+        return customerMapper.selectByPrimaryKey(id);
+    }
+
+    public CustomerInfo getCustomerInfo(int id) {
+        CustomerInfoExample example = new CustomerInfoExample();
+        CustomerInfoExample.Criteria criteria = example.createCriteria();
+        criteria.andIdEqualTo(id);
+        List<CustomerInfo> customerInfoList = customerInfoMapper.selectByExample(example);
+        if(customerInfoList.size()!=0)
+        return customerInfoList.get(0);
+        else {
+            return null;
+        }
+
+    }
 }
