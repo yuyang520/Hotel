20711add38d086f13e7fa06e6d90d4d914b4e2c8
525438796 <525438796@qq.com>
2019-07-26 15:00:28 +0800

    添加管理员进行订单修改的页面

--- src/main/java/com/javaee/hotel/controller/MOrderController.java
+++ src/main/java/com/javaee/hotel/controller/MOrderController.java
@@ -8,10 +8,8 @@ import com.javaee.hotel.service.MOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,34 @@ public class MOrderController {
         System.out.println(mOrderService.orderlistRespond());
         return mOrderService.orderlistRespond();
     }
+
+    @GetMapping("/add")
+    public String goOrderPage(Model model){
+        model.addAttribute("orderList",new OrderList());
+        return "order-add";
+    }
+
+
+
+    @PostMapping("/add")
+    public String saveorder(OrderList orderList){
+        mOrderService.saveorder(orderList);
+
+        return "redirect:/MOrderlist";
+    }
+
+    @GetMapping("/edit")
+    public String goorderEditPage(@RequestParam("orderId") String orderId, Model model){
+         OrderList orderList = mOrderService.findOrderListByPrimaryKey(orderId);
+         model.addAttribute("orderList",orderList);
+        return "order-add";
+    }
+
+
+//    @GetMapping()
+//    public String deleteOrer(@RequestParam("orderId") String orderId){
+//        mOrderService.deleteOrderById(orderId);
+//        return "redirect:/MOrderlist";
+//    }
 
 }
--- src/main/java/com/javaee/hotel/service/MOrderService.java
+++ src/main/java/com/javaee/hotel/service/MOrderService.java
@@ -12,8 +12,27 @@ import java.util.List;
 public class MOrderService {
     @Autowired
     private OrderListMapper orderListMapper;
+
     public List<OrderList> orderlistRespond(){
         OrderListExample example = new OrderListExample();
         return orderListMapper.selectByExample(example);
+    }
+
+
+    public Integer saveorder(OrderList orderList){
+        if(orderList.getOrderId() == null){
+            return  orderListMapper.insert(orderList);
+        }else {
+            return orderListMapper.updateByPrimaryKey(orderList);
+        }
+
+    }
+
+    public OrderList findOrderListByPrimaryKey(String orderId){
+        return orderListMapper.selectByPrimaryKey(orderId);
+    }
+
+    public void deleteOrderById(String orderId) {
+       orderListMapper.deleteByPrimaryKey(orderId);
     }
 }
