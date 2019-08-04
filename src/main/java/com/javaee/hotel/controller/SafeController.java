f4b69b5e841029907b9138b333b0e3e663d3bf9d
Taifendesune <1970024754@qq.com>
2019-08-04 14:25:12 +0800

    邮箱和电话绑定、修改、解绑界面

--- src/main/java/com/javaee/hotel/controller/SafeController.java
+++ src/main/java/com/javaee/hotel/controller/SafeController.java
@@ -30,12 +30,67 @@ public class SafeController {
         model.addAttribute("userInfo",customerInfo);
         return "safecenter";
     }
+
     @GetMapping(value = "/modifypwd")
     public String gomPwdPage(HttpSession session){
         if(session.getAttribute("id")==null) {
             return "redirect:/welcome";
         }
         return "changepwd";
+    }
+
+    @GetMapping(value = "/modifytel")
+    public String gomTelPage(HttpSession session){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        return "changetel";
+    }
+
+    @GetMapping(value = "/modifyemail")
+    public String gomEmailPage(HttpSession session){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        return "changeemail";
+    }
+
+    @GetMapping(value = "/settel")
+    public String gosTelPage(HttpSession session){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        return "settel";
+    }
+
+    @GetMapping(value = "/setemail")
+    public String gosEmailPage(HttpSession session){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        return "setemail";
+    }
+
+    @GetMapping(value = "/unsettel")
+    public String gouTelPage(HttpSession session,Model model){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        int id = Integer.parseInt(session.getAttribute("id").toString());
+        CustomerInfo customerInfo = userService.getNowUser(id).get(0);
+        model.addAttribute("userInfo",customerInfo);
+        return "unsettel";
+    }
+
+    @GetMapping(value = "/unsetemail")
+    public String gouEmailPage(HttpSession session,Model model){
+        if(session.getAttribute("id")==null) {
+            return "redirect:/welcome";
+        }
+        int id = Integer.parseInt(session.getAttribute("id").toString());
+        CustomerInfo customerInfo = userService.getNowUser(id).get(0);
+        model.addAttribute("userInfo",customerInfo);
+        return "unsetemail";
     }
 
     public String hidetel(String tel){
